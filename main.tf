b99a5360cc071b5832022bfe0fb75c038e3f7287
Oscar Ivan <o@maleficarum.mx>
2025-04-13 08:19:55 -0600

    Add public load balancer definition

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_security_group" "ecs_tasks" {
     protocol        = "tcp"
     from_port       = 80
     to_port         = 80
-    security_groups = [aws_security_group.alb.id]
+    security_groups = [aws_security_group.public_security_group.id]
   }
 
   egress {
