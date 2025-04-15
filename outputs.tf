b401dd595bd8adcfd596dae9758ecd4e91c0406c
Oscar Ivan <o@maleficarum.mx>
2025-04-14 19:35:43 -0600

    Fix project structure

---
+++ outputs.tf
@@ -0,0 +1,29 @@
+output "public_subnets" {
+  value = aws_subnet.public_subnets[*].id
+  description = "Public subnets"
+}
+
+output "private_subnets" {
+  value = aws_subnet.private_subnets[*].id
+  description = "Public subnets"
+}
+
+output "ecs_tasks" {
+  value = aws_security_group.ecs_tasks.id
+  description = "ECS tasks list"
+}
+
+output "target_group_arn" {
+  value = aws_alb_target_group.public_app_target_group.arn
+  description = "APRN for ALB"
+}
+
+output "domain_name" {
+  value = aws_alb.public_main_alb.dns_name
+  description = "ALB public endpoint"
+}
+
+output "public_alb_dns_name" {
+  value = aws_alb.public_main_alb.dns_name
+  description = "Public DNS name on LB"
+}
---
+++ public-lb.tf
@@ -0,0 +1,73 @@
+# nosemgrep:terraform.aws.security.aws-elb-access-logs-not-enabled.aws-elb-access-logs-not-enabled
+resource "aws_alb" "public_main_alb" {
+  name               = "ecs-alb"
+  subnets            = aws_subnet.public_subnets[*].id
+  security_groups    = [aws_security_group.public_security_group.id]
+  #tfsec:ignore:aws-elb-alb-not-public
+  internal           = false 
+  load_balancer_type = "application"
+  drop_invalid_header_fields = true
+  
+  tags = {
+    Name = "ecs-alb"
+  }
+}
+
+# Target Group
+resource "aws_alb_target_group" "public_app_target_group" {
+  name        = "ecs-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.ecs_vpc.id
+  target_type = "ip"
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    interval            = 30
+    path                = "/"
+    matcher             = "200-399"
+  }
+}
+
+# Listener
+resource "aws_alb_listener" "public_http" {
+  load_balancer_arn = aws_alb.public_main_alb.arn
+  port              = "80"
+  #tfsec:ignore:aws-elb-http-not-used
+  # nosemgrep:terraform.aws.security.insecure-load-balancer-tls-version.insecure-load-balancer-tls-version
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.public_app_target_group.arn
+  }
+}
+
+# ALB Security Group
+resource "aws_security_group" "public_security_group" {
+  name        = "alb-sg"
+  description = "Allow HTTP traffic to ALB"
+  vpc_id      = aws_vpc.ecs_vpc.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    #tfsec:ignore:aws-ec2-no-public-ingress-sgr
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    #tfsec:ignore:aws-ec2-no-public-egress-sgr
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  tags = {
+    Name = "alb-sg"
+  }
+}
